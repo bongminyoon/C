e82b050f0aec9ee85933e093169638759839d9b4
bongminyoon <162547615+bongminyoon@users.noreply.github.com>
2024-10-30 10:46:26 +0900

    Add files via upload

---
+++ basic43/Project35/2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age = 0;
+	int chest = 0;
+	scanf("%d", &age);
+	scanf("%d", &chest);
+
+	if (age < 20)
+	{
+		if (chest < 85)
+		{
+			printf("나이가 %d세이고 사이즈는 S입니다.", age);
+		}
+		else if (85 <= chest && chest < 95)
+		{
+			printf("나이가 %d세이고 사이즈는 M입니다.", age);
+		}
+		else
+		{
+			printf("나이가 %d세이고 사이즈는 L입니다.", age);
+		}
+
+	}
+	else
+	{
+		if (chest < 85)
+		{
+			printf("나이가 %d세이고 사이즈는 S입니다.", age);
+		}
+		else if (85 <= chest && chest < 95)
+		{
+			printf("나이가 %d세이고 사이즈는 M입니다.", age);
+		}
+		else
+		{
+			printf("나이가 %d세이고 사이즈는 L입니다.", age);
+		}
+
+	}
+
+	return 0;
+
+}
+		
