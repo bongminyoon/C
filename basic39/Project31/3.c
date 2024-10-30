cb7b1a24aed073ffaa4a993a8a8451139736a0db
bongminyoon <162547615+bongminyoon@users.noreply.github.com>
2024-10-30 10:44:17 +0900

    Add files via upload

---
+++ basic39/Project31/3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int* pa, int* pb);
+
+int main(void)
+{
+	int a = 20;
+	int b = 30;
+	
+	swap(&a, &b);
+	printf("%d %d", a, b);
+	
+	return 0;
+}
+
+void swap(int* pa, int* pb)
+{
+	int temp;
+	
+	temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+}
---
+++ basic39/Project31/4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+double fine_max(double* pa, int size);
+int main(void)
+{
+	double ary[] = { 2.5, 3.1, 5.5, 7.1, 4.6, 9.9, 1.5 };
+	double max = 0;
+	double* a;
+	a = ary;
+	size = 
+
+	max = double fine_max(double* a, int size);
+	printf("배열의 최대값 : %d", max);
+
+}
+double fine_max(double* pa, int size)
+{
+	
+	//for (i = 0; i < 8; i++)
+	{
+		if ( >max)max = 
+	}
+		
+	return max;
+	
+ }
