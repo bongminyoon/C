63210e0813b2f2941ef924dfc9d9bb57c446bfd7
bongminyoon <162547615+bongminyoon@users.noreply.github.com>
2024-10-30 10:32:15 +0900

    Add files via upload

---
+++ basic17/0423/FileName.c
@@ -0,0 +1,58 @@
+	/*#include<stdio.h>
+
+	int main(void)
+	{
+		int i;
+		int j;
+	
+
+		for (i = 0; i < 5; i++)
+		{
+			for (j = 0; j < 5; j++)
+			{
+				if (i == 0 || i == 4 || j == 0 || j == 4)
+				{
+					printf(" * ");
+				}
+				else
+				{
+					printf("  ");
+				}
+		
+			}
+			printf("\n");	
+
+		}
+		return 0;
+	}
+	*/
+/*#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+	int tot;
+
+	for (i = 1; i < 6; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (i == 1)
+			{
+				tot = i + (1 * 0);
+
+				printf("%d", tot);
+			}
+			else if (i == 2)
+			{
+				printf(" ");
+			}
+			else if (i == 3)
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}*/
---
+++ basic17/0423/FileName2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+	int tot;
+
+	for (i = 6; i > 1; i--)
+	{
+		for (j = 1; j < i; j++) //1 5
+		{
+		if
+		}
+		else if
+		{
+			tot = i + (1 * j);
+			printf("%d", tot);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
